fc75130e8b5d37fe6b1bfa08826afba93125c856
ApplePieCompany <caaaq107@hcn.zaq.ne.jp>
2017-06-28 12:06:04 +0900

    ItemVIew Label Implement

--- RealItemList/Controller/ItemViewController.swift
+++ RealItemList/Controller/ItemViewController.swift
@@ -14,7 +14,6 @@ class ItemViewController: UIViewController {
 	var chainCode : String = "8a15262b11be5966f5816ecd52f6a177f1dcf3392892ea950f691874af1a7d6657afc03e87edbfe76a8fd7a4082680b2bfc4e836a3127aa656ab72b7651788c5"
 	var chaincode_url : String = "https://76c87932401b41fd959e90733cd0954d-vp0.us.blockchain.ibm.com:5004/chaincode"
 	var method_func = ["method":"query","func":"read"]
-	var args = "A101"
 	
 	var name: AnyObject? {
 		get {return UserDefaults.standard.object(forKey: "name") as AnyObject}
@@ -32,26 +31,32 @@ class ItemViewController: UIViewController {
 
 		self.view.backgroundColor = UIColor.white
 
+		// CollectionView
+		let _itemModel : ItemModel = ItemModel(_name: name as! String,_hashkey: hashKey as! String)
+		_itemModel.imageView.frame = CGRect(x: 50, y: 50, width: self.view.bounds.width/2, height: self.view.bounds.height/2)
+		_itemModel.imageView.contentMode = UIViewContentMode.scaleAspectFill
+		self.view.addSubview(_itemModel.imageView)
+		
 		let chainCodeAccessModel = ChainCodeAccessModel()
 		chainCodeAccessModel.url = self.chaincode_url
 		chainCodeAccessModel.chainCode = self.chainCode
 		chainCodeAccessModel.method = self.method_func["method"]
 		chainCodeAccessModel.funcs = self.method_func["func"]
-		chainCodeAccessModel.key = self.args
+		chainCodeAccessModel.key = self.hashKey as! String
 		chainCodeAccessModel.makeParams()
 		
 		//QUERY CHAIN CODE
 		let alamoController = AlamoController()
 		alamoController.getAlamofire(model:chainCodeAccessModel) { responseObject, error in
 			let json = JSON(responseObject!)
-			print(json["result"])
+			let param = json["result"]["message"]
+			
+			//			_itemModel.setOwner(_para: param as AnyObject)
+
+			let _ser : String = "Serial : \(json["result"]["message"]["ser"])"
+			self.view.addSubview(self.makeLabel(_x: 10,_y: self.view.bounds.height/2+10,_w: 100,_h: 20,_t: _ser))
+			
 		}
-		
-		// CollectionView
-		let _itemModel : ItemModel = ItemModel(_name: name as! String,_hashkey: hashKey as! String)
-		_itemModel.imageView.frame = CGRect(x: 50, y: 50, width: self.view.bounds.width/2, height: self.view.bounds.height/2)
-		_itemModel.imageView.contentMode = UIViewContentMode.scaleAspectFill
-		self.view.addSubview(_itemModel.imageView)
 	}
 	
 	override func didReceiveMemoryWarning() {
@@ -59,6 +64,15 @@ class ItemViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 	
+	
+	func makeLabel(_x:CGFloat,_y:CGFloat,_w:CGFloat,_h:CGFloat,_t:String)->UILabel{
+		let _return: UILabel = UILabel(frame: CGRect(x: _x, y: _y, width: _w, height: _h))
+		_return.textColor = UIColor.black
+		_return.text = _t
+		_return.textAlignment = NSTextAlignment.right
+		_return.adjustsFontSizeToFitWidth = true
+		return _return
+	}
 	
 	/*
 	// MARK: - Navigation
--- RealItemList/Models/ItemModel.swift
+++ RealItemList/Models/ItemModel.swift
@@ -15,6 +15,9 @@ class ItemModel: NSObject {
 	var name:String = ""
 	var hashkey:String = ""
 	var imageView:UIImageView!
+
+	var serial:String!
+	var upd:Date!
 	
 	convenience override init(){
 		self.init()
@@ -26,5 +29,19 @@ class ItemModel: NSObject {
 		self.imageView = UIImageView()
 		self.imageView.frame = CGRect(x: 2, y: 2, width: 50, height: 50)
 		self.imageView.image = UIImage(named:_name)! as UIImage
+	}
+	
+	func setOwner(_para:AnyObject){
+		var dic : [String:String] = [:]
+		let sp = _para.components(separatedBy: ",")
+		for ele in sp{
+			let csp = ele.components(separatedBy: ":")
+			dic[csp[0]]=csp[1]
+		}
+		
+		self.serial = dic["ser"]
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy/MM/dd"
+		self.upd = formatter.date(from: dic["upd"]!)!
 	}
 }
